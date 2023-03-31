01dab84f38a18c29bad1f4cbde1a04b741798bfe
Ivo196 <ivo.tonioni@gmail.com>
2023-03-31 18:13:06 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import employeeRoutes from './routes/employees.routes.js'
+import indexRoutes from './routes/index.routes.js'
+
+const app = express();
+
+app.use(express.json())
+
+app.use('/api',employeeRoutes)
+app.use(indexRoutes)
+
+app.use((req,res,next)=> {
+    res.status(404).json({
+        message:'Endpoint not found'
+    })
+})
+
+app.listen(3000);
+console.log("Server running on port 3000");
